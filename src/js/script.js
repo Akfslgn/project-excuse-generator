6f720e59401e5c35b342d7c0faf8b743621c19ee
Akfslgn <akifsalgn@gmail.com>
2024-11-24 04:09:03 +0000

    update

--- src/js/script.js
+++ src/js/script.js
@@ -8,14 +8,31 @@ window.onload = function () {
 
     // 2. Generate a random excuse
 
-    const part1 = [ 'one', 'two', 'three'];
-    const part2 = [ 'four', 'five', 'six'];
+    const subjects = [ 
+         "My cat", "The neighbor's dog", "A talking cactus", "An alien", "The refrigerator", 
+        "My lost sock", "The washing machine", "My pizza", "A friendly ghost", "The old chair"
+    ];
+    const verbs = [ 
+        "stole", "destroyed", "hid", "ran away with", "turned into", 
+        "ate", "sang to", "danced around", "painted", "teleported"
+    ];
+    const objects = [ 
+        "my homework", "my favorite mug", "my phone", "my shoes", "the remote control", 
+        "the house keys", "a slice of pizza", "my headphones", "my book", "my glasses"
+    ];
+    const endings = [ 
+        "while I was sleeping.", "before I could stop it.", "just to annoy me!", 
+        "and then disappeared.", "and left me a note.", "as a prank.", "and laughed!", 
+        "while I was on a call.", "and ran outside.", "then vanished into thin air."
+    ];
 
-    const indexPart1 = Math.floor(Math.random() * part1.length);
-    const indexPart2 = Math.floor(Math.random() * part2.length);
+    const indexSubjects = Math.floor(Math.random() * subjects.length);
+    const indexVerbs = Math.floor(Math.random() * verbs.length);
+    const indexObjects = Math.floor(Math.random() * objects.length);
+    const indexEndings = Math.floor(Math.random() * endings.length);
 
     
-    const excuseText = `${part1[indexPart1]} ${part2[indexPart2]}`;
+    const excuseText = `${subjects[indexSubjects]} ${verbs[indexVerbs]} ${objects[indexObjects]} ${endings[indexEndings]}`;
 
 
 
